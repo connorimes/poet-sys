7fde369b8f86546bf4ef413f1253fd4f75fd263c
Connor Imes <connor.k.imes@gmail.com>
2016-02-14 17:40:44 -0600

    Fix whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ use libc::{c_void, c_char, c_int, c_uint, c_double};
 
 /// Typedef for an "apply" function - used to manipulate system or application settings.
 pub type poet_apply_func = extern fn(states: *mut c_void,
-	                                 num_states: c_uint,
-	                                 id: c_uint,
-	                                 last_id: c_uint);
+                                     num_states: c_uint,
+                                     id: c_uint,
+                                     last_id: c_uint);
 
 /// Typedef for "current state" function - used to try and determine the current system or
 /// application state.
@@ -40,39 +40,39 @@ extern {
     // Core functions - poet.h
 
     pub fn poet_init(perf_goal: c_double,
-	                 num_system_states: c_uint,
-	                 control_states: *mut poet_control_state_t,
-	                 apply_states: *mut poet_cpu_state_t,
-	                 apply_func: poet_apply_func,
-	                 curr_state_func: poet_curr_state_func,
-	                 period: c_uint,
-	                 buffer_depth: c_uint,
-	                 log_filename: *const c_char) -> *mut poet_state;
+                     num_system_states: c_uint,
+                     control_states: *mut poet_control_state_t,
+                     apply_states: *mut poet_cpu_state_t,
+                     apply_func: poet_apply_func,
+                     curr_state_func: poet_curr_state_func,
+                     period: c_uint,
+                     buffer_depth: c_uint,
+                     log_filename: *const c_char) -> *mut poet_state;
 
     pub fn poet_apply_control(state: *mut poet_state,
-	                          id: u64,
-	                          perf: c_double,
-	                          pwr: c_double);
+                              id: u64,
+                              perf: c_double,
+                              pwr: c_double);
 
     pub fn poet_destroy(state: *mut poet_state);
 
     // Configuration/utility functions - poet_config.h
 
     pub fn apply_cpu_config(states: *mut c_void,
-	                        num_states: c_uint,
-	                        id: c_uint,
-	                        last_id: c_uint);
+                            num_states: c_uint,
+                            id: c_uint,
+                            last_id: c_uint);
 
     pub fn get_control_states(path: *const c_char,
-	                          states: *mut *mut poet_control_state_t,
-	                          num_states: *mut c_uint) -> c_int;
+                              states: *mut *mut poet_control_state_t,
+                              num_states: *mut c_uint) -> c_int;
 
     pub fn get_cpu_states(path: *const c_char,
-	                      states: *mut *mut poet_cpu_state_t,
-	                      num_states: *mut c_uint) -> c_int;
+                          states: *mut *mut poet_cpu_state_t,
+                          num_states: *mut c_uint) -> c_int;
 
     pub fn get_current_cpu_state(states: *const c_void,
-	                             num_states: c_uint,
-	                             curr_state_id: *mut c_uint) -> c_int;
+                                 num_states: c_uint,
+                                 curr_state_id: *mut c_uint) -> c_int;
 
 }
